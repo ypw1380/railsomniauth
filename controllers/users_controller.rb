6ef86fd3946d576182f01ab8183200b12a47fda6
ypw1380 <ypw1380@gmail.com>
2014-04-09 04:48:22 +0000

    adjust

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class UsersController < ApplicationController
   before_filter :authenticate_user!
   
   def index
--- models/user.rb
+++ models/user.rb
@@ -6,22 +6,13 @@ class User < ActiveRecord::Base
   
   devise :omniauthable
   
-  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    if user
-      return user
-    else
-      registered_user = User.where(:email => auth.info.email).first
-      if registered_user
-        return registered_user
-      else
-        user = User.create(name:auth.extra.raw_info.name,
-                            provider:auth.provider,
-                            uid:auth.uid,
-                            email:auth.info.email,
-                            password:Devise.friendly_token[0,20],)
-      end 
-    end    
+  def self.find_for_facebook_oauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      end     
   end 
 end
 
